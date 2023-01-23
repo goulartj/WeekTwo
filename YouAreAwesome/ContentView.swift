2738240a3585ca60edf86fefc4d7300dce256849
John J Goulart <goulartj@bc.edu>
2023-01-23 13:57:31 -0500

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by John Goulart on 1/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var messageString = ""
+    var body: some View {
+        VStack {
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.yellow)
+                .padding()
+            
+            HStack {
+                Button("Awesome") {
+                    //This is the action performed when the button is pressed
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
