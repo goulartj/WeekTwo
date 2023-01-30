4235aea0ce8deeca3479e180fb1cc2a97397fc08
John J Goulart <goulartj@bc.edu>
2023-01-30 13:13:39 -0500

    Initial Commit

---
+++ WeekTwo/WeekTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeekTwoApp.swift
+//  WeekTwo
+//
+//  Created by John Goulart on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct WeekTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
