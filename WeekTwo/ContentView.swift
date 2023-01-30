cb7047dddf3f831c374442153fd4c0b1456e9b58
John J Goulart <goulartj@bc.edu>
2023-01-30 13:35:08 -0500

    1.6

--- WeekTwo/ContentView.swift
+++ WeekTwo/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = "When the Genius Bar needs help, they call you!"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .fill(
+                    Gradient(colors: [Color("SkyBlue"), .orange])
+                )
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("You Have Skills!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(Color("BC-Gold"))
+                    .padding()
+                    .background(Color("BC-Maroon"))
+                    .cornerRadius(15)
+                
+                Spacer()
+                
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                //                .border(.orange, width: 1)
+                    .padding()
+                
+                Spacer()
+                
+                Divider()
+                    .padding()
+                
+                HStack {
+                    Button("Awesome") {
+                        messageString = "You Are Awesome!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Spacer()
+                    
+                    Button("Great") {
+                        messageString = "You Are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+    //            .border(.purple, width: 5)
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
