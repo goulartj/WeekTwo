00c0863505c822a199f8889594aff34848064c9f
John J Goulart <goulartj@bc.edu>
2023-02-13 13:27:18 -0500

    1.26!!

--- WeekTwo/ContentView.swift
+++ WeekTwo/ContentView.swift
@@ -17,53 +17,71 @@ struct ContentView: View {
     @State private var lastImageNumber = -1
     @State private var lastSoundNumber = -1
     @State private var audioPlayer: AVAudioPlayer!
+    @State private var soundIsOn = true
     
     
     var body: some View {
-     
-            VStack {
+        
+        VStack {
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 30)
+                .padding()
+                .animation(.default, value: messageString)
+            
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .animation(.easeInOut(duration: 0.15), value: messageString)
+            
+            Spacer()
+            
+            HStack {
                 
-                Image(imageName)
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(30)
-                    .shadow(radius: 30)
-                    .padding()
-                    
+                Text("Sound On")
+                Toggle("", isOn: $soundIsOn)
+                    .labelsHidden()
+                    .onChange(of: soundIsOn) { _ in
+                        if audioPlayer != nil && audioPlayer .isPlaying {
+                            audioPlayer.stop()
+                        }
+                    }
+                
                 Spacer()
+                
+                Button("Show Message") {
+                    let messages = ["You Are Awesome!",
+                                    "You Are Great!",
+                                    "You Are Fantastic!",
+                                    "Fabulous? That's You!"]
                     
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color.red)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-                    .padding()
+                    lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber, upperBound: messages.count-1)
+                    messageString = messages[messageNumber]
                     
-                Spacer()
+                    lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBound: 9)
+                    imageName = "image\(lastImageNumber)"
                     
-                HStack {
-                    Button("Show Message") {
-                        let messages = ["You Are Awesome!",
-                                        "You Are Great!",
-                                        "You Are Fantastic!",
-                                        "Fabulous? That's You!"]
-                        
-                        lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber, upperBound: messages.count-1)
-                        messageString = messages[messageNumber]
-                        
-                        lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBound: 9)
-                        imageName = "image\(lastImageNumber)"
-                        
-                        lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber, upperBound: 5)
+                    lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber, upperBound: 5)
+                    if soundIsOn {
                         playSound(soundName: "sound\(lastSoundNumber)")
                     }
-                    .buttonStyle(.borderedProminent)
                 }
-                .padding()
+                .buttonStyle(.borderedProminent)
+            }
+            .tint(.accentColor)
         }
+        .padding()
     }
     
     func nonRepeatingRandom(lastNumber: Int, upperBound:Int) -> Int {
